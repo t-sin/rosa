18bc349d8ad4b7699a76d1af099d3ad05a2e5ce0
t-sin <shinichi.tanaka45@gmail.com>
2016-04-27 00:13:14 +0900

    Add name parser

--- src/rosa.lisp
+++ src/rosa.lisp
@@ -1,6 +1,17 @@
 (in-package :cl-user)
 (defpackage rosa
-  (:use :cl))
+  (:use :cl
+        :proc-parse))
 (in-package :rosa)
 
-;; blah blah blah.
+
+(defun parse-name (line)
+  (with-string-parsing (line)
+    (when (match? ":")
+      (bind (name (skip-until (lambda (c) (char= c #\space))))
+        (if (eofp)
+            (return-from parse-name name)
+            (progn
+              (advance)
+              (bind (str (skip-while (lambda (c) t)))
+                (return-from parse-name (cons name str)))))))))
