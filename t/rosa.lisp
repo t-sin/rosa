a33097451c531784cbe00c306a230aedad92f087
t-sin <shinichi.tanaka45@gmail.com>
2016-05-01 23:52:24 +0900

    Add basic test

--- t/rosa.lisp
+++ t/rosa.lisp
@@ -7,8 +7,56 @@
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :rosa)' in your Lisp.
 
-(plan nil)
+(plan 1)
 
-;; blah blah blah.
+(defvar *test-string* "
+
+for default context
+
+:title Rosa - Text parts with metadata
+:author Shinichi TANAKA
+:date 2016-05-01
+
+:date date2
+
+ignored
+
+
+:abstract
+
+Rosa is a simple markup language for text parts with metadata.
+
+:basis
+
+
+Rosa's *named text* is a pair of strings, consists of **name** and **text**.
+
+**Name** is a metadata.
+And **text** is a data named with **name**.
+**Text** is just one line string, or is multi line strings.
+
+...
+
+;comment
+
+phew, english... I'm tired now...
+
+")
+
+(is (with-input-from-string (in *test-string*)
+      (peruse-from-stream in))
+    '(:|basis| ("Rosa's *named text* is a pair of strings, consists of **name** and **text**.
+
+**Name** is a metadata.
+And **text** is a data named with **name**.
+**Text** is just one line string, or is multi line strings.
+
+...")
+      :|abstract| ("Rosa is a simple markup language for text parts with metadata.")
+      :|date| ("2016-05-01" "date2")
+      :|author| ("Shinichi TANAKA")
+      :|title| ("Rosa - Text parts with metadata")
+      :|+nil+| ("for default context")))
+
 
 (finalize)
