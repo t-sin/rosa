2855cdc73a5d9a04079dda0b72f6fb96736b1302
t-sin <shinichi.tanaka45@gmail.com>
2016-12-24 19:17:59 +0900

    Some fixes

--- t/rosa.lisp
+++ t/rosa.lisp
@@ -45,11 +45,11 @@ We can consider **Label** as *key* and **body** as *value*.
 (is (with-input-from-string (in *test-string*)
       (peruse in))
     (let ((hash (make-hash-table)))
-      (setf (gethash :|title| #("Rosa - Named text parts")))
-      (setf (gethash :|author| #("Shinichi TANAKA")))
-      (setf (gethash :|date| #("2016-05-01" "2016-12-21")))
-      (setf (gethash :|abstract| #("Rosa is a text labeling language.")))
-      (setf (gethash :|body| #("Rosa is a language give key-value structure to text.
+      (setf (gethash :|title| hash) #("Rosa - text labeling language"))
+      (setf (gethash :|author| hash) #("Shinichi TANAKA"))
+      (setf (gethash :|date| hash) #("2016-05-01" "2016-12-21"))
+      (setf (gethash :|abstract| hash) #("Rosa is a text labeling language."))
+      (setf (gethash :|body| hash) #("Rosa is a language give key-value structure to text.
 In other words, rosa is a language that give one name to text block.
 
 Text written in rosa represent a ordered set of key-value pair.
@@ -59,16 +59,16 @@ Here, one pair in the set, it consist of **label** and **body**.
 We can consider **Label** as *key* and **body** as *value*.
 
 :key value
-;phew, engrish... I'm tired now...")))
+;phew, engrish... I'm tired now..."))
       hash)
     :test #'equalp)
 
 ;;; peruse API; return eazy-to-use structure
 (is (with-input-from-string (in *test-string*)
       (peruse-as-plist in))
-    '(:|title| "Rosa - Named text parts"
+    '(:|title| "Rosa - text labeling language"
       :|author| "Shinichi TANAKA"
-      :|date| "2016-05-01" "2016-12-21"
+      :|date| ("2016-05-01" "2016-12-21")
       :|abstract| "Rosa is a text labeling language."
       :|body| "Rosa is a language give key-value structure to text.
 In other words, rosa is a language that give one name to text block.
@@ -102,9 +102,10 @@ We can consider **Label** as *key* and **body** as *value*.
                                :end (+ start (length string1))))))
   (is (with-input-from-string (in *test-string*)
         (rosa::skim in))
-      `(:|title| ,(determine-position "Rosa - Named text parts" *test-string*)
+      `(:|title| ,(determine-position "Rosa - text labeling language" *test-string*)
          :|author| ,(determine-position "Shinichi TANAKA" *test-string*)
-         :|date| ,(determine-position "2016-05-01" "2016-12-21" *test-string*)
+         :|date| '(,(determine-position "2016-12-21" *test-string*)
+                   ,(determine-position "2016-12-21" *test-string*))
          :|abstract| ,(determine-position "Rosa is a text labeling language." *test-string*)
          :|body| ,(determine-position "Rosa is a language give key-value structure to text.
 In other words, rosa is a language that give one name to text block.
