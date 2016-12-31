ef6ecc39c1f8f1b099839ed671f326f70dba0a49
t-sin <shinichi.tanaka45@gmail.com>
2016-12-31 15:35:27 +0900

    Indexing, picking and perusing as plist

--- src/rosa.lisp
+++ src/rosa.lisp
@@ -13,13 +13,19 @@
 
 (defun peruse-as-plist (stream)
   "read key-value data as plist."
-  )
-
+  (let ((data (peruse stream)))
+    (loop
+       :for k :being :each :hash-keys :of data :using (:hash-value v)
+       :nconc (list k v))))
 
 (defun index (stream)
   "returns all keys in `stream`."
-  )
+  (let ((data (peruse stream)))
+    (loop
+       :for k :being :each :hash-keys :of data
+       :collect k)))
 
 (defun pick (stream label)
   "returns value corresponded `label`."
-  )
+  (let ((data (peruse stream)))
+    (gethash label data)))
