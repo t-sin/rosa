b7d10069a8718505669ce1532893357b6df8be5d
t-sin <shinichi.tanaka45@gmail.com>
2016-12-31 17:13:28 +0900

    `peruse` returns vector ;p

--- t/semantics.lisp
+++ t/semantics.lisp
@@ -26,11 +26,11 @@
 
   (subtest "inline label is represent as regexp `^:([a-z0-9][a-z0-9-]*) (.+)$`"
     (perusing-test ":abcd is read as label"
-        '(:|abcd| "is read as label"))
+        '(:|abcd| #("is read as label")))
     (perusing-test ":abcd-efg is read as label"
-        '(:|abcd-efg| "is read as label"))
+        '(:|abcd-efg| #("is read as label")))
     (perusing-test ":abcd- is read as label"
-        '(:|abcd-| "is read as label")))
+        '(:|abcd-| #("is read as label"))))
 
   (subtest "these are not inline labels"
     (perusing-test ":-abcd is not read as label" nil)
@@ -40,18 +40,19 @@
     (perusing-test ":abCD is not read as label" nil))
 
   (subtest "body can include any characters except line-break"
-    (perusing-test ":label body" '(:|label| "body"))
-    (perusing-test ":label golden-body" '(:|label| "golden-body"))
-    (perusing-test ":label heart of gold" '(:|label| "heart of gold"))
-    (perusing-test (format nil ":label so long~%and thanks for all the fish") '(:|label| "so long"))
+    (perusing-test ":label body" '(:|label| #("body")))
+    (perusing-test ":label golden-body" '(:|label| #("golden-body")))
+    (perusing-test ":label heart of gold" '(:|label| #("heart of gold")))
+    (perusing-test (format nil ":label so long~%and thanks for all the fish")
+                   '(:|label| #("so long")))
 
     (subtest "special cases with labels"
       (perusing-test (format nil ":label so long~%:label2 and thanks for all the fish")
-          '(:|label| "so long"
-            :|label2| "and thanks for all the fish"))
+          '(:|label| #("so long")
+            :|label2| #("and thanks for all the fish")))
       (perusing-test (format nil ":label so long~%:label2~%and thanks for all the fish")
-          '(:|label| "so long"
-            :|label2| "and thanks for all the fish")))))
+          '(:|label| #("so long")
+            :|label2| #("and thanks for all the fish"))))))
 
 (subtest "block labels"
   (diag "block label is consists of two parts; label line and following body line(s)")
@@ -64,11 +65,11 @@
 
   (subtest "block label is represent as regexp `^:([a-z0-9][a-z0-9-]*)$`"
     (perusing-test (format nil ":abcd~%is read as label")
-        '(:|abcd| "is read as label"))
+        '(:|abcd| #("is read as label")))
     (perusing-test (format nil ":abcd-efg~%is read as label")
-        '(:|abcd-efg| "is read as label"))
+        '(:|abcd-efg| #("is read as label")))
     (perusing-test (format nil ":abcd-~%is read as label")
-        '(:|abcd-| "is read as label")))
+        '(:|abcd-| #("is read as label"))))
 
   (subtest "these are not label"
     (perusing-test (format nil ":-abcd~%is not read as label") nil)
@@ -78,21 +79,21 @@
     (perusing-test (format nil ":abCD~%is not read as label") nil))
 
   (subtest "body can include any lines except both kind of labels"
-    (perusing-test (format nil ":label~%body") '(:|label| "body"))
+    (perusing-test (format nil ":label~%body") '(:|label| #("body")))
     (perusing-test (format nil ":label~%golden~%body")
-        `(:|label| ,(format nil "golden~%body")))
+        `(:|label| #(,(format nil "golden~%body"))))
     (perusing-test (format nil ":label~%heart of gold")
-        '(:|label| "heart of gold"))
+        '(:|label| #("heart of gold")))
     (perusing-test (format nil ":label~%so long~%and thanks for all the fish")
-        `(:|label| ,(format nil "so long~%and thanks for all the fish"))))
+        `(:|label| #(,(format nil "so long~%and thanks for all the fish")))))
 
   (subtest "special cases with labels"
       (perusing-test (format nil ":label~%so long~%:label2 and thanks for all the fish")
-          '(:|label| "so long"
-            :|label2| "and thanks for all the fish"))
+          '(:|label| #("so long")
+            :|label2| #("and thanks for all the fish")))
       (perusing-test (format nil ":label~%so long~%:label2~%and thanks for all the fish")
-          '(:|label| "so long"
-            :|label2| "and thanks for all the fish"))))
+          '(:|label| #("so long")
+            :|label2| #("and thanks for all the fish")))))
 
 (subtest "comment"
   (subtest "comment starts with colon, and are ignored"
@@ -101,13 +102,13 @@
 
   (subtest "comments in block body are ignored"
     (perusing-test (format nil ":block~%oh,~%;comment~%deep thought.")
-        `(:|block| ,(format nil "~%oh,~%deep thought.")))
+        `(:|block| #(,(format nil "~%oh,~%deep thought."))))
     (perusing-test (format nil ":block~%oh,~%;comment1~%;comment2~%deep thought.")
-        `(:|block| ,(format nil "~%oh,~%deep thought.")))
+        `(:|block| #(,(format nil "~%oh,~%deep thought."))))
     (perusing-test (format nil ":block~%oh,~%;comment1~%deep~%;comment2~%thought.")
-        `(:|block| ,(format nil ":block~%oh,~%deep~%thought.")))
+        `(:|block| #(,(format nil ":block~%oh,~%deep~%thought."))))
     (perusing-test (format nil ":block~%oh,~%;comment1~%deep thought.~%;comment2")
-        `(:|block| ,(format nil ":block~%oh,~%deep thought.")))))
+        `(:|block| #(,(format nil ":block~%oh,~%deep thought."))))))
 
 (subtest "escape sequences"
   (subtest "colon escaping"
@@ -116,13 +117,13 @@
 
     (subtest "colon escaping in block label"
       (perusing-test (format nil ":block~%:: is colon")
-          `(:|block| ,(format nil ": is colon")))
+          `(:|block| #(,(format nil ": is colon"))))
 
       (subtest "escaping is elable only at head of line"
         (perusing-test (format nil ":block~%::: is colon colon")
-            `(:|block| ,(format nil ":: is colon colon")))
+            `(:|block| #(,(format nil ":: is colon colon"))))
         (perusing-test (format nil ":block~% :: is colon colon")
-            `(:|block| ,(format nil " :: is colon colon"))))))
+            `(:|block| #(,(format nil " :: is colon colon")))))))
 
   (subtest "semicolon escaping"
     (subtest "escaping is a plain line"
@@ -130,22 +131,22 @@
 
     (subtest "semicolon escaping in block label"
       (perusing-test (format nil ":block~%:; is semicolon")
-          `(:|block| ,(format nil "; is semicolon")))
+          `(:|block| #(,(format nil "; is semicolon"))))
 
       (subtest "escaping is elable only at head of line"
         (perusing-test (format nil ":block~%:;; is semicolon semicolon")
-            `(:|block| ,(format nil ";; is semicolon semicolon")))
+            `(:|block| #(,(format nil ";; is semicolon semicolon"))))
         (perusing-test (format nil ":block~% ;; is semicolon semicolon")
-            `(:|block| ,(format nil " ;; is semicolon semicolon")))))))
+            `(:|block| #(,(format nil " ;; is semicolon semicolon"))))))))
 
 (subtest "plain line not in block are ignored"
-  (perusing-test (format nil ":label text~%foo") '(:|label| "text"))
-  (perusing-test (format nil "foo~%:label text") '(:|label| "text"))
+  (perusing-test (format nil ":label text~%foo") '(:|label| #("text")))
+  (perusing-test (format nil "foo~%:label text") '(:|label| #("text")))
   (perusing-test (format nil "foo~%:label text~%bar") '(:|label| "text"))
 
-  (perusing-test (format nil ":label~%text~%foo") '(:|label| "text"))
-  (perusing-test (format nil "foo~%:label~%text") '(:|label| "text"))
-  (perusing-test (format nil "foo~%:label~%text~%bar") '(:|label| "text")))
+  (perusing-test (format nil ":label~%text~%foo") '(:|label| #("text")))
+  (perusing-test (format nil "foo~%:label~%text") '(:|label| #("text")))
+  (perusing-test (format nil "foo~%:label~%text~%bar") '(:|label| #("text"))))
 
 
 (finalize)
